efd9695e0067ceab7dd362c719922a222dcd519a
kennedy <ifeanyi@exqu.co>
2024-03-11 01:59:02 +0100

    patched the routes in erver.ts

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import App from '@/app';
 import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
-import { ProductRoute } from '@routes/index.route';
+import { IndexRoute } from '@routes/index.route';
 
 ValidateEnv();
 
-const app = new App([new AuthRoute(), new UserRoute(), new ProductRoute()]);
+const app = new App([new AuthRoute(), new UserRoute(), new IndexRoute()]);
 
 app.listen();
